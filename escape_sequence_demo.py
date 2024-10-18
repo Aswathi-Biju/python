cd838860b1e93d1e375c5eec1bb1a3e510fcefa6
Aswathi-Biju <aswathibiju8118@gmail.com>
2024-10-18 16:27:55 +0530

    escape_sequence_demo.py

---
+++ escape_sequence_demo.py
@@ -0,0 +1,12 @@
+'''
+Author: Aswathi Biju
+Date: 18/10/2024
+Description: program to print the following string in a specific format (see the output).
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+'''
+print("Twinkle,\ttwinkle,\tlittle star,\n\t\tHow I wonder what you are! \n\t\t\t\tUp above the world so high, \n\t\t\t\tLike a diamond in the sky. \nTwinkle,\ttwinkle,\tittle star,\n\t\tHow I wonder what you are")
