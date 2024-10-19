768b2c9331e3721abb2baebd9b192b1df7eeebf1
Aswathi-Biju <aswathibiju8118@gmail.com>
2024-10-19 14:24:36 +0530

    CalculatorDemo.py

---
+++ CalculatorDemo.py
@@ -0,0 +1,16 @@
+'''
+Author: Aswathi Biju
+Date: 19/10/2024
+Description: Simple desktop calculator using Python. Only the five basic arithmetic operators.
+'''
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+num3=int(input("Enter a number:"))
+print("The sum of num1 and num2 is:",num1+num2)
+print("The difference when num2 is subtracted from num1 is:",num1-num2)
+print("The product of num1 and num2 is:",num1*num2)
+print("The quotient when num1 is divided by num2 is:",num1/num2)
+print("The remainder when num1 is divided by num2 is:",num1%num2)
+print("The result of (num1 + num2) * num3 / 2 is:",(num1+num2)*num3/2)
+
+
