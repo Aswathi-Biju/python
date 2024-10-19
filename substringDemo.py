9d9a59b2c77732a3762ea085b8694efb674a8b0d
Aswathi-Biju <aswathibiju8118@gmail.com>
2024-10-19 14:36:14 +0530

    Create substringDemo.py

---
+++ substringDemo.py
@@ -0,0 +1,12 @@
+'''
+Author: Aswathi Biju
+Date: 19/10/2024
+Description: program that create, concatenate,
+and print a string and access a sub-string from a given string.
+'''
+str1=input("Enter your first name: ")
+str2=input("Enter your last name: ")
+str3= str1+" "+str2
+print(str3)
+sub_str=str3[5:8]
+print(sub_str)
