055772ebf9c0ae1984e0c3c75711f17f2e6d8cb5
Aswathi-Biju <aswathibiju8118@gmail.com>
2024-10-25 14:44:05 +0530

    Create logicDemo.py

---
+++ logicDemo.py
@@ -0,0 +1,13 @@
+'''
+Author: Aswathi Biju
+Date: 25/10/2024
+Description: program to find the largest of three numbers.
+The program should take three numbers as input from the user and determine which one is the largest.
+Use conditional statements to compare the numbers.
+'''
+num1= int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+num3= int(input("Enter third number: "))
+largest_no= num1 and num2 and num3
+print("The largest number is: ", largest_no)
+
