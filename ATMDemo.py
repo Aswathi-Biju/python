209f054630f22611836bf85cdd66eb6a82c0463c
Aswathi-Biju <aswathibiju8118@gmail.com>
2024-10-28 10:02:04 +0530

    ATMDemo.py

--- ATMDemo.py
+++ ATMDemo.py
@@ -1,3 +1,14 @@
+'''
+Author: Aswathi Biju
+Date: 28/10/2024
+Description: program that simulates a simple bank ATM system.
+The user has an initial balance of $1000.
+The ATM should display a menu with options to:
+    1.Check Balance
+    2.Deposit Money
+    3.Withdraw Money
+    4.Exit
+'''
 balance_amount=1000
 while True:
     print("\n1. \tCheck Balance")
@@ -15,6 +26,10 @@ while True:
         withdraw_amount= float(input("Enter the amount to withdraw: "))
         print(f"The amount to withdraw ${withdraw_amount}"
               f"The current balance ${balance_amount}")
+        if withdraw_amount>balance_amount:
+            print("Insufficient Amount")
+        else :
+            print("Sufficient Amount")
     elif choice ==4:
         break
     else :
