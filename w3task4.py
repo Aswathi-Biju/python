9465e8813664482b60ae503024936222293af039
Aswathi Biju <aswathibiju8118@gmail.com>
2024-11-30 16:19:41 +0530

    Create w3task4.py

---
+++ w3task4.py
@@ -0,0 +1,10 @@
+"""
+4. Write a Python program to reverse a string.
+Sample String : "1234abcd"
+Expected Output : "dcba4321"
+"""
+def reversing_a_string():
+    string= input("Enter a string: ")
+    string=string[::-1]
+    print("Reversing the string: ",string)
+reversing_a_string()
